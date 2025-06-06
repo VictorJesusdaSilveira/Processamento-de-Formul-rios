8d9280d514def544d3bc658473adcce1e9668192
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2025-06-06 11:53:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function processarFormulario(){
+    const nome = document.querySelector("#nome").value
+    const data_nascimento = document.querySelector("#data_nascimento").value
+    const email = document.querySelector("#email").value
+    const telefone = document.querySelector("#telefone").value
+    const cpf = document.querySelector("#CPF").value
+    const genero = document.querySelector("#genero").value
+    const habilidades = document.querySelector("#habilidades").value
+    const pais = document.querySelector("#pais").value
+    const comentarios = document.querySelector("#comentarios").value
+
+    return false
+}
+
+if (condition) {
+    
+}
